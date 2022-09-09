10c2b140a3049fe242dff99b54bcd9215f7cbf2c
andinianst93 <andinianst93@gmail.com>
2022-09-10 01:41:43 +0700

    about page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -32,6 +32,7 @@ const AboutPage = () => {
             Beatae, maxime maiores? Quos, necessitatibus laborum sapiente at ad
             aperiam aut asperiores.
           </p>
+          <p>This is projects from udemy.</p>
         </article>
       </Wrapper>
     </main>
